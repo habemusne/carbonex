47070975477c6390cd19b02a4c07783dfce1f050
Nan Chen <itisnanful@gmail.com>
2015-03-02 19:51:32 -0800

    ss

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+
+var app = express();
+var courses = require('./www/routes/courses')
+
+app.set('views', './www/templates');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/www/'));
+//app.set('view engine', 'jade');
+
+//Routes
+app.get('/', function(req, res) {
+  res.render('home.html', {
+    title: 'Welcome'
+  });
+});
+
+app.get('/api/getcourses', courses.getCourses);
+
+app.listen(process.env.PORT || 3000);
