6cd5d4c58e862f4bad256ae7cedae0f18b3654c6
Nan Chen <itisnanful@gmail.com>
2015-03-02 20:16:12 -0800

    ss

--- web.js
+++ web.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
  
-app.use(express.static(__dirname + "/www/"));
+app.use(express.static("./www/"));
+app.use(express.static("./"));
 app.listen(process.env.PORT || 5000);
